565349961b335676c1e67e3390d3e0d37fa86913
karyna.avetsisian <avetisyan.k.workmail@gmail.com>
2018-01-11 11:50:38 +0300

    rating filter

--- specs/test_ratingFilter/spec_setPrice.js
+++ specs/test_ratingFilter/spec_setPrice.js
@@ -2,23 +2,49 @@ let FilterPanel = require(`../../pages/filters/FilterRestaurantsPanel`);
 let FilterList = require(`../../pages/filters/FilterListRestaurant`);
 
 describe('test for price rating', () => {
+    beforeEach(() => {
+        let filterPanel = new FilterPanel();
 
-    let TEST_PRICES = [1, 2, 3, 4, 5];
+        filterPanel.clearRadioFilter(`Rating`)
+            .then(() => filterPanel.clearRadioFilter(`Price`))
+            .then(() => filterPanel.clearCheckFilter())
+    });
+
+    afterEach(() => {
+        let filterPanel = new FilterPanel();
+
+        filterPanel.clearRadioFilter(`Rating`)
+            .then(() => filterPanel.clearRadioFilter(`Price`))
+            .then(() => filterPanel.clearCheckFilter())
+    });
+
+    let TEST_RATING = [1, 2, 3, 4, 5];
 
     it('should set price', () => {
-
         let filterPanel = new FilterPanel();
         let filterList = new FilterList();
-        filterPanel.clearRadioFilter(`Rating`)
-            .then(() => filterPanel.clearCheckFilter())
-            .then(() => {
-                TEST_PRICES.forEach((testPrice, index) => {
 
-                    filterPanel.clearRadioFilter('Price')
-                        .then(() => filterPanel.setRatingFilter(`Price`, index))
-                        .then(() => filterList.getAllSelectedPrices()
-                            .each((price) => expect(filterList.getRatingLevel(price)).toEqual(Number.parseInt(testPrice))))
-                });
-            });
+        TEST_RATING.forEach((testPrice, index) => {
+
+            filterPanel.setRatingFilter(`Price`, index)
+                .then(() => filterList.getAllSelectedPrices()
+                    .each((price) => expect(filterList.getRatingLevel(price)).toEqual(Number.parseInt(testPrice))))
+        });
+    });
+
+    it('should set rating', () => {
+        let filterPanel = new FilterPanel();
+        let filterList = new FilterList();
+
+        TEST_RATING.forEach((testRating, index) => {
+
+            filterPanel.setRatingFilter(`Rating`, index)
+                .then(() => filterList.getAllSelectedRatings()
+                    .each((rating) => expect(filterList.getRatingLevel(rating)).toEqual(Number.parseInt(testRating))))
+        });
     });
 });
+
+
+
+

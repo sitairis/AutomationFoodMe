9b3127b67bf2a5b71715bcb947d7f23b68ebb9d5
karyna.avetsisian <avetisyan.k.workmail@gmail.com>
2018-01-22 15:59:42 +0300

    amendment

--- pages/AuthPage.js
+++ pages/AuthPage.js
@@ -1,7 +1,9 @@
 let log = require('../lib/Logger');
+let utils = require('../lib/utils');
 
 class AuthPage {
     constructor() {
+        this.className = 'AuthPage';
         this.inputName = element(by.model('customerName'));
         this.inputAddress = element(by.model('customerAddress'));
         this.btnFindRestaurants = $('[ng-click="findRestaurants(customerName, customerAddress)"]');
@@ -9,8 +11,8 @@ class AuthPage {
     }
 
     navigate() {
-        return browser.get('http://localhost:5000/#/customer')
-            .then(() => log.step('AuthPage', 'navigate','open AuthPage'));
+        return browser.get('#/customer')
+            .then(() => log.step(this.className, 'navigate','open AuthPage'));
     }
 
     authorizate(name, address) {
@@ -21,18 +23,21 @@ class AuthPage {
     }
 
     fillInputName(name) {
-        return this.inputName.sendKeys(name)
-            .then(() =>  log.step('AuthPage', 'fillInputName',`type deliver's name`));
+        return utils.doSendKeys(this.inputName, name, `type deliver's name`)
+            .then(() =>  log.step(this.className, 'fillInputName',`type deliver's name`))
+            .catch(() => Promise.reject(`${this.className} : Error --- fillInputName`));
     }
 
     pressFindRestaurantsButton() {
-        return this.btnFindRestaurants.click()
-            .then(() => log.step('AuthPage', 'pressFindRestaurantsButton',`click on button 'Find Restaurants'`));
+        return utils.doClick(this.btnFindRestaurants, `click on button 'Find Restaurants'`)
+            .then(() => log.step(this.className, 'pressFindRestaurantsButton',`click on button 'Find Restaurants'`))
+            .catch(() => Promise.reject(`${this.className} : Error --- pressFindRestaurantsButton`));
     }
 
     fillInputAddress(address) {
-        return this.inputAddress.sendKeys(address)
-            .then(() => log.step('AuthPage', 'fillInputAddress',`type deliver's address`));
+        return utils.doSendKeys(this.inputAddress, address, `type deliver's address`)
+            .then(() => log.step(this.className, 'fillInputAddress',`type deliver's address`))
+            .catch(() => Promise.reject(`${this.className} : Error --- fillInputAddress`));
     }
 }
 module.exports = AuthPage;

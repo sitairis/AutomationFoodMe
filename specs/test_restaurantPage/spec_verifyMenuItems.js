a1182c51c111bd796c7fcc88f25453d6759cb4e5
karyna.avetsisian <avetisyan.k.workmail@gmail.com>
2018-01-30 15:40:46 +0300

    spec_verifyMenuItems

--- specs/test_restaurantPage/spec_verifyMenuItems.js
+++ specs/test_restaurantPage/spec_verifyMenuItems.js
@@ -19,16 +19,21 @@ describe('test for restaurant page', () => {
 
         log.testStep('test for restaurant page', 1, 'open restaurant');
         mainPage.openRestaurant(utils.getRandomNumber(0, 38))
-            .then(() => restaurantPage.getMenuObjArray())
+            .then(() => {
+                log.testStep('test for restaurant page', 2, 'get menu');
+                return restaurantPage.getMenuObjArray()
+            })
             .then((objArray) => utils.getMenuInfoObjArray(objArray))
             .then((menuItems) => {
                 restaurantPage.getRestaurantName()
-                    .then((restName) =>  restInfoWithDetails.info.find((a)=> a.name === restName))
+                    .then((restName) => {
+                        log.testStep('test for restaurant page', 3, 'get name of restaurant');
+                        return restInfoWithDetails.info.find((a) => a.name === restName)
+                    })
                     .then((rest) => {
-                        log.testStep('test for restaurant page', 4, 'verify price');
+                        log.testStep('test for restaurant page', 4, 'verify menu');
                         expect(menuItems).toEqual(rest.menuItems.map((item) => `${item.name}${item.price}`.toLowerCase()));
                     })
-
             })
     });
 });

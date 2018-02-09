f78231eb9406b874645a7cecd568166ea124ce50
karyna.avetsisian <avetisyan.k.workmail@gmail.com>
2018-02-09 11:40:24 +0300

    add init function in conf.js

--- conf.js
+++ conf.js
@@ -1,32 +1,46 @@
-exports.config = {
-    framework: 'jasmine',
-    seleniumAddress: 'http://localhost:4444/wd/hub',
-    baseUrl: "http://localhost:5000/#/",
-    specs: ['./specs/*/*.js'],
-    capabilities: {
-        shardTestFiles: true,
-        maxInstances: 3,
-        browserName: 'chrome',
-        chromeOptions: {
-            args: ['headless', 'disable-gpu']
+/**
+ * init function
+ * @param config
+ * @returns {*}
+ */
+let init = config => {
+    let spec = null;
+
+    let match = parseProcessArgv(process.argv);
+
+    match.forEach(objProp => {
+        switch (objProp.prop) {
+            case 'specs':
+                spec = objProp.value;
+                break;
+            case 'browser':
+                config.capabilities.browserName = objProp.value;
+                if (objProp.value !== 'chrome' && objProp.value !== 'firefox')
+                    config.directConnect = false;
+                break;
+            case 'timeout':
+                config.jasmineNodeOpts.defaultTimeoutInterval = parseInt(objProp.value);
+                break;
+            case 'threads':
+                config.capabilities.maxInstances = Number.parseInt(objProp.value);
+                config.capabilities.shardTestFiles = config.capabilities.maxInstances > 1;
+                config.capabilities.chromeOptions = {
+                    args: ['headless', 'disable-gpu']
+                };
+                break;
         }
-    },
-  /*  suites: {
-        checkout: './specs/test_checkoutPage/*.js',
-        order: './specs/test_thankYouPage/*.js',
-        restPage: './specs/test_restaurantPage/spec_verifyMenuItems.js',
-        rating: './specs/test_ratingFilter/*.js',
-        checkbox: './specs/test_checkboxFilter/*.js',
-        all: './specs/*/
-    // },
-    onPrepare: function() {
+    });
+
+    config.specs.push(parseSpecPath(spec));
+
+    config.onPrepare = () => {
         let AllureReporter = require('jasmine-allure-reporter');
         jasmine.getEnv().addReporter(new AllureReporter({
             resultsDir: 'allure-results'
         }));
-    },
+    };
 
-    beforeLaunch: () => {
+    config.beforeLaunch = () => {
         const request = require("request");
         const fs = require('fs');
 
@@ -56,5 +70,57 @@ exports.config = {
                 log.step('', '', "File restaurants.json has been created");
             });
         });
+    };
+
+    return config;
+};
+/**
+ * вернет путь для тестов
+ * @param spec
+ * @returns {string}
+ */
+let parseSpecPath = spec => {
+    let specPath = splitFunc(spec, ',');
+
+    if (specPath[0] === '*') {
+        return `./specs/*/*.js`;
+    } else {
+        let dirs = splitFunc(spec, '|')
+            .map(dir => dir.endsWith(',') ? dir.slice(0, -1) : dir);
+
+        return dirs.map(dir => dir.endsWith('.js') ? `./specs/${dir}` : `./specs/${dir}/*.js`);
     }
 };
+/**
+ * вернет значения из командной строки
+ * @param processArgv
+ */
+let parseProcessArgv = processArgv => splitFunc(`${processArgv}`,'--params')
+        .map(el => splitFunc(el,'='))
+        .filter(el => el.length > 1)
+        .map(el => {
+            return {
+                prop: el[0].slice(1),
+                value: el[1]
+            }
+        });
+/**
+ * делает split
+ * @param str
+ * @param delim
+ */
+let splitFunc = (str, delim) => str.split(delim);
+
+exports.config = (function() {
+    return init({
+        specs: [],
+        framework: 'jasmine',
+        seleniumAddress: 'http://localhost:4444/wd/hub',
+        baseUrl: "http://localhost:5000/#/",//ip
+        capabilities: {
+            browserName: 'chrome',
+            shardTestFiles: false,
+            maxInstances: 1
+        }
+    });
+})();
